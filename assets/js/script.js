b532971d5ca67516471f26dd132e863f6517dcd6
hiroxxx03 <115777709+hiroxxx03@users.noreply.github.com>
2023-02-16 11:17:42 +0900

    add page-top

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-$(function() {
-  $('.hamburger').click(function() {
+$(function () {
+  $('.hamburger').click(function () {
       $(this).toggleClass('active');
 
       if ($(this).hasClass('active')) {
@@ -8,4 +8,24 @@ $(function() {
           $('.header-sp-menu').removeClass('active');
       }
   });
+});
+
+$(function () {
+  let pageTop = $('.page-top-button');
+  pageTop.hide();
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    console.log(scroll)
+    if(scroll > 500) {
+      pageTop.fadeIn()
+    }else{
+      pageTop.fadeOut()
+    }
+  })
+  pageTop.click(function () {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 500)
+    return false;
+  })
 });
